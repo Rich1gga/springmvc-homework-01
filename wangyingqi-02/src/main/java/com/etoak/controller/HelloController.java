a16abf8d23d75773a43dcb37719b990f0408ae29
yingqi.wang <etoak@qq.com>
2020-08-13 01:25:03 +0800

    wangyingqi

--- wangyingqi-02/src/main/java/com/etoak/controller/HelloController.java
+++ wangyingqi-02/src/main/java/com/etoak/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController implements HttpRequestHandler {
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = request.getParameter("name");
         request.setAttribute("result","Hello "+name);
-        request.getRequestDispatcher("/hello1.jsp").forward(request,response);
+        request.getRequestDispatcher("/hello.jsp").forward(request,response);
     }
 }
